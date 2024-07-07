8a5bbeb009578ad872d3628ffa9833d3c5f7a9db
Ivan <vany4golos@yandex.ru>
2024-07-07 23:50:54 +0300

    new component

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+import './Projects.css';
+
+function Projects () {
+  return (
+    <section className="projects">
+      <h2 className='projects__title'>Projects</h2>
+    </section>
+  )
+}
+
+export default Projects;
