946e0f80e009f334c10bb4f99749ed3e43086b1d
Ivan <vany4golos@yandex.ru>
2024-07-07 23:15:00 +0300

    fix const

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,43 @@
 import './Header.css';
 
 function Header({ isDark, toogleButton }) {
+  const links = [
+    {
+      href: '#',
+      title: 'Home',
+    },
+    {
+      href: '#',
+      title: 'Home',
+    },
+    {
+      href: '#',
+      title: 'About',
+    },
+    {
+      href: '#',
+      title: 'Projects',
+    },
+    {
+      href: '#',
+      title: 'Contact',
+    },
+  ]
   return (
     <header className={`header ${isDark ? 'header_dark' : ''}`}>
       <section className={`header__container ${isDark ? 'header__container_dark' : ''}`}>
         <div className="header__logo"></div>
         <nav className='header__nav'>
-          <a href="#" className={`header__link ${isDark ? 'header__link_color_dark' : ''}`}>Home</a>
-          <a href="#" className={`header__link ${isDark ? 'header__link_color_dark' : ''}`}>About</a>
-          <a href="#" className={`header__link ${isDark ? 'header__link_color_dark' : ''}`}>Skills</a>
-          <a href="#" className={`header__link ${isDark ? 'header__link_color_dark' : ''}`}>Projects</a>
-          <a href='#' className={`header__link ${isDark ? 'header__link_color_dark' : ''}`}>Contact</a>
+          {links.map((a, i) => {
+            return (
+              <a
+                href={a.href}
+                key={i}
+                className={`header__link ${isDark ? 'header__link_color_dark' : ''}`}>
+                {a.title}
+              </a>
+            )
+          })}
         </nav>
         <button onClick={toogleButton} className={`header__button ${isDark ? 'header__button_dark' : ''}`}>
           <div className={`header__svg ${isDark ? 'header__svg_dark' : ''}`}></div>
@@ -21,3 +48,11 @@ function Header({ isDark, toogleButton }) {
 }
 
 export default Header;
+
+/*
+          <a href="#" className={`header__link ${isDark ? 'header__link_color_dark' : ''}`}>Home</a>
+          <a href="#" className={`header__link ${isDark ? 'header__link_color_dark' : ''}`}>About</a>
+          <a href="#" className={`header__link ${isDark ? 'header__link_color_dark' : ''}`}>Skills</a>
+          <a href="#" className={`header__link ${isDark ? 'header__link_color_dark' : ''}`}>Projects</a>
+          <a href='#' className={`header__link ${isDark ? 'header__link_color_dark' : ''}`}>Contact</a>
+          */
