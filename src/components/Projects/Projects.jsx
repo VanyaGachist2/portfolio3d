ca2eea7bfce7323ebd07eb594483137e8d6dc83f
Ivan <vany4golos@yandex.ru>
2024-07-08 13:50:41 +0300

    test grid

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,34 @@
+import TitleMain from '../TitleMain/TitleMain';
 import './Projects.css';
+import meet from '../../assets/images/встреча.png';
 
 function Projects () {
+  const textForProjects = {
+    titleMain: 'Projects',
+  }
   return (
     <section className="projects">
-      <h2 className='projects__title'>Projects</h2>
+      <article className='projects__container'>
+        <TitleMain name={textForProjects.titleMain} />
+          <ul className='projects__list'>
+            <li className='projects__div'>
+              <img className='projects__img' alt='#' src={meet} />
+            </li>
+            <li className='projects__div'>
+              <img className='projects__img' alt='#' src={meet} />
+            </li>
+            <li className='projects__div'>
+              <img className='projects__img' alt='#' src={meet} />
+            </li>
+            <li className='projects__div'>
+              <img className='projects__img' alt='#' src={meet} />
+            </li>
+            <li className='projects__div'>
+              <img className='projects__img' alt='#' src={meet} />
+            </li>
+          </ul>
+      </article>
+
     </section>
   )
 }
