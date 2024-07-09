0c65c9522431f70843fd907b683b4061cc7e8b00
Ivan <vany4golos@yandex.ru>
2024-07-09 13:49:49 +0300

    add const for cards

---
+++ src/utils/constForCards.js
@@ -0,0 +1,60 @@
+import firstImageForFirstProject from '../assets/images/1project.png';
+import secondImageForFirstProject from '../assets/images/1projecttwo.png';
+
+export const cards = [
+  {
+    id: 1,
+    images: {
+      firstImage: firstImageForFirstProject,
+      secondImage: secondImageForFirstProject,
+    },
+    title: `Проект "Movie-Explorer"`,
+    subtitle: `Проект по поиску фильмов по названию.
+    Функционал: Поисковой фильтр, лайк фильма, регистрация,
+    авторизация, редактирование профиля. Стек технологий:
+    JavaScript; React; NodeJs; Express; CSS3.`,
+  },
+  {
+    id: 2,
+    images: {
+      firstImage: firstImageForFirstProject,
+      secondImage: secondImageForFirstProject,
+    },
+    title: `Проект "Mesto"`,
+    subtitle: `Сайт – берущий идею публикаций фото как в instagram.
+    Функционал: Публикация фото, Регистрация и Авторизация,
+    Смена Никнейма, и аватара, постановка снятие лайка.
+    Технологии: JavaScript, React, CSS3, Rest Api, NodeJs, Express,
+    MongoDb.`
+  },
+  {
+    id: 3,
+    images: {
+      firstImage: firstImageForFirstProject,
+      secondImage: secondImageForFirstProject,
+    },
+    title: `Проект “Russia”`,
+    subtitle: `Обычный сайт про путишествие в России.
+    Его основная идея это адаптивная верстка под разные
+    разрешения экрана c использованием media запросов.
+    Стек технологий: HTML5; CSS3.`
+  },
+  {
+    id: 4,
+    images: {
+      firstImage: firstImageForFirstProject,
+      secondImage: secondImageForFirstProject,
+    },
+    title: `Проект "Vlog"`,
+    subtitle: `В разработке!`,
+  },
+  {
+    id: 5,
+    images: {
+      firstImage: firstImageForFirstProject,
+      secondImage: secondImageForFirstProject,
+    },
+    title: `Проект "TORT"`,
+    subtitle: `В разработке!`,
+  }
+]
