8d7f2bf200709d9c468e071b17d4e917f4e2d8dc
Ivan <vany4golos@yandex.ru>
2024-07-09 17:10:46 +0300

    card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,19 @@
 import './Card.css';
-import testOne from '../../assets/images/y7zI5l7gfhM';
-import testTwo from '../../assets/images/встреча';
 
-function Card ({ openCard }) {
+function Card ({ openCard, card, onClose }) {
   return (
     <div className={`card ${openCard ? 'card_opened' : ''}`}>
       <div className='card__container'>
         <ul className='card__list'>
           <li className='card__card'>
-            <img className='card__image' alt='#' src={testOne} />
+            <img className='card__image' alt='#' src={card?.images.firstImage} />
           </li>
         </ul>
-        <h2 className='card__heading'>Test</h2>
-        <p className='card__description'></p>
-        <button className='card__close'></button>
+        <h2 className='card__heading'>{card?.title}</h2>
+        <p className='card__description'>
+        {card?.subtitle}
+        </p>
+        <button onClick={onClose} className='card__close'></button>
       </div>
     </div>
   )
