15652558b5b253cd784338a7b2287b68b963b564
Ivan <vany4golos@yandex.ru>
2024-07-09 13:06:21 +0300

    Contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,32 @@
+import TitleMain from '../TitleMain/TitleMain';
+import './Contact.css';
+
 function Contact () {
   return (
     <section className="contact">
       <article className="contact__container">
-        <h1 className="contact__title">Contact</h1>
+        <TitleMain name='Contact' />
+        <form className='contact__form'>
+          <input
+            type='text'
+            className='contact__input'
+            placeholder='Name'
+            />
+          <input
+            type='text'
+            className='contact__input'
+            placeholder='Email'
+            />
+          <textarea
+            className='contact__area'
+            placeholder='Who are you?'
+            ></textarea>
+          <div className='contact__buttons'>
+            <a href='https://vk.com/ibolvan123' className='contact__link contact__link_vk'></a>
+            <a href='#' className='contact__link contact__link_github'></a>
+          </div>
+          <button type='submit' className='contact__button'>Отправить</button>
+        </form>
       </article>
     </section>
   )
