a09b09f3f9c0cb0fc84d3ff0e46589821e3cc384
Ivan <vany4golos@yandex.ru>
2024-07-14 00:31:28 +0300

    delete project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,10 @@ import Header from './Header/Header'
 import Home from './Home/Home'
 import Skills from './Skills/Skills'
 import Projects from './Projects/Projects'
-import Card from './Card/Card';
 import Footer from './Footer/Footer'
-
-import { cards } from '../utils/constForCards'
 
 function App() {
   const [ isDark, setIsDark ] = useState(false);
-  const [ isCardOpen, setIsCardOpen ] = useState(null);
 
   const toogleButton = () => {
     const changeTheme = !isDark ? 'dark' : 'light';
@@ -27,22 +23,13 @@ function App() {
     }
   }, []);
 
-  const openCardModal = useCallback((card) => {
-    setIsCardOpen({ card })
-  }, []);
-
-  const closeCardModal = () => {
-    setIsCardOpen(null);
-  }
-
   return (
     <main className='app'>
-      <Card card={isCardOpen} onClose={closeCardModal} />
       <Header isDark={isDark} toogleButton={toogleButton} />
       <Home homeDark={isDark} />
       <About aboutDark={isDark} isTrue={isDark} />
       <Skills darkSkills={isDark} />
-      <Projects projectDark={isDark} onClick={openCardModal} card={cards} />
+      <Projects projectDark={isDark} />
       <Contact />
       <Footer />
     </main>
