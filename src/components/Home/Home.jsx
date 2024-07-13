eba90abf8b69c59a11b08f6585b5b7b04555e2cc
Ivan <vany4golos@yandex.ru>
2024-07-14 00:29:21 +0300

    add birthday

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,12 +3,21 @@ import Three from './Three';
 import { Canvas } from '@react-three/fiber';
 
 function Home ({ homeDark }) {
+  let currentAge = 20;
+  const currentDay = new Date();
+  const getDay = currentDay.getDate();
+  const getMonth = currentDay.getMonth();
+  if (getDay === 5 && getMonth === 11) {
+    currentAge += 1;
+  }
+
   return (
     <section className={`home ${homeDark ? 'home_dark' : ''}`}>
       <article className={`home__container ${homeDark ? 'home__container_dark' : ''}`}>
         <div className="home__message">
           <h1 className="home__title">@Vanya</h1>
-          <p className='home__web'>20 years. shutnik</p>
+          <p className='home__web'>
+            {currentAge}. shutnik</p>
           <p className='home__web'>gamer.</p>
           <p className='home__second'>all for developers.</p>
         </div>
