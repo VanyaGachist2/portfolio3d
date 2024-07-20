02479886f9a53f7699621f9d250a70e0f567cf3b
Ivan <vany4golos@yandex.ru>
2024-07-20 22:38:02 +0300

    add text

---
+++ src/utils/constForText.js
@@ -0,0 +1,71 @@
+const header = [
+  {
+    en: [
+      {
+        href: '#',
+        title: 'Home',
+      },
+      {
+        href: '#',
+        title: 'About',
+      },
+      {
+        href: '#',
+        title: 'Skills',
+      },
+      {
+        href: '#',
+        title: 'Projects',
+      },
+      {
+        href: '#',
+        title: 'Contact',
+      },
+    ],
+    ru: [
+      {
+        href: '#',
+        title: 'Главная',
+      },
+      {
+        href: '#',
+        title: 'Обо мне',
+      },
+      {
+        href: '#',
+        title: 'Скилы',
+      },
+      {
+        href: '#',
+        title: 'Проекты',
+      },
+      {
+        href: '#',
+        title: 'Контакты',
+      },
+    ]
+  }
+];
+
+const home = [
+  {
+    en: [
+      {
+        title: "// Hello World!",
+        subtitle: `My name is Vanya and i'm a front-end developer
+            from Crimea, Russia. I love coding, I love coding, I love coding, I love coding, I love coding
+            I love coding, I love coding, I love coding, I love coding, I love coding, I love coding, I love coding,
+            I love coding, I love coding, I love coding, I love coding.`,
+      }
+    ],
+    ru: [
+      {
+        title: "// Привет Мир!",
+        subtitle: `My name is Vanya and i'm a front-end developer
+            from Crimea, Russia. I love coding, I love coding, I love coding, I love coding, I love coding
+            I love coding, I love coding, I love coding, I love coding, I love coding, I love coding, I love coding,
+            I love coding, I love coding, I love coding, I love coding.`,
+      }
+    ]
+  }
+]
